339b3f98543ccdf7e485d0c187e3b55597c6338d
Dhrumil1808 <dhrml.shah@gmail.com>
2015-04-23 02:08:29 +0530

    committted

--- dbrecharge.php
+++ dbrecharge.php
@@ -1,6 +1,6 @@
 <?php
-mysql_connect("localhost","root","") or die("cannot connect to server");
-mysql_select_db("recharge") or die("database not found");
-//mysql_connect("166.62.8.51","instantrecharge","Erecharge@123") or die("cannot connect to server");
-//mysql_select_db("irecharge") or die("database not found");
+//mysql_connect("localhost","root","") or die("cannot connect to server");
+//mysql_select_db("recharge") or die("database not found");
+mysql_connect("sql5.freemysqlhosting.net","sql574925","uZ1%iI1*") or die("cannot connect to server");
+mysql_select_db("sql574925") or die("database not found");
 ?>
